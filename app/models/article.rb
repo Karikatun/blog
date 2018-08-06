2a8fa40b4b17ed150f7f5e9ec158c1094a02311d
Anton <shevchenko-anton2014@yandex.ru>
2018-08-06 16:21:21 +0300

    Added basic authentication

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, :text, presence: true, length: {minimum: 5}
+  validates :title, uniqueness: true
 end
